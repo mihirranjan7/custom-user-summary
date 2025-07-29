6a947f7846c0f0381734107ecce65a72a86ca0fc
mihirranjan7 <mihirranjan730@gmail.com>
2025-07-29 13:19:35 +0530

    Delete javascripts/discourse/components/javascripts/discourse/components directory

--- javascripts/discourse/components/javascripts/discourse/components/profile-note-display.js
+++
@@ -1,7 +0,0 @@
-import Component from "@glimmer/component";
-
-export default class ProfileNoteDisplay extends Component {
-  get profileNote() {
-    return this.args.user?.user_fields?.["5"];
-  }
-}
